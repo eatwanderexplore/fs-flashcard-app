2514de11ad7e8863ae4f31b167da1931928c058a
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-09-23 12:52:40 +1200

    boilerplate homepage

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine','ejs');
+app.get('/', function (req, res){
+ res.render("home");
+ });
+
+ 
 app.listen(3000);
 console.log('Node app is running on port 3000');

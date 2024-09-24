2dde676e6caa0c3685c92bbad481afc4df56e5f7
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-09-24 13:57:59 +1200

    connected my sql database, created login page

--- app.js
+++ app.js
@@ -1,16 +1,63 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const session = require('express-session');
+const path = require('path');
+const conn = require('./dbConfig');
 
 app.set('view engine','ejs');
+
+app.use(session({
+    secret: 'yoursecret',
+    resave: true,
+    saveUninitialized: true
+}));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 // define routes
 app.get('/', function (req, res){
  res.render("home");
+ });
+
+app.get('/login', function(req, res){
+    res.render("login");
+})
+
+// login user
+app.post('/auth', function(req, res) {
+    let name = req.body.username;
+    let password = req.body.password;
+    if (name && password) {
+        conn.query('SELECT * FROM user WHERE username = ? AND password_hash = ?', [name, password],
+        function(error, results, fields){
+            if (error) throw error;
+            if (results.length > 0) {
+                req.session.loggedin = true;
+                req.session.username = name;
+                // need to add if user is admin redirect to admin dashboard else redirect to student dashboard
+                res.redirect('/admindashboard');
+            } else {
+                res.send('Incorrect Username and/or Password!');
+            }
+            res.end();
+        });
+    } else {
+        res.send('Please enter Username and Password!');
+        res.end();
+    }
+ });
+
+ // users can access if logged in
+ app.get('/admindashboard', function (req, res, next) {
+    if (req.session.loggedin) {
+        res.render('admindashboard');
+    } else {
+        res.send('Please login to view this page!');
+    }
  });
 
 // Start the server
